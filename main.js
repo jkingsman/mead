420cf82bde6b3ec3fd1ddf533ad0b2887bcc316e
jkingsman <jack.kingsman@gmail.com>
2017-10-09 20:01:55 -0700

    Initial commit.

---
+++ main.js
@@ -0,0 +1,99 @@
+// fields that should NOT be automatically filled
+const nonAutofillFields = ['ingredients', 'timeline', 'gravReadings'];
+
+// draw a given batch into the modal
+function drawBatch(batch) {
+    $("[id^='meadData_']").each(function(elem) {
+        $(this).empty();
+    });
+
+    // draw autofills -- fields whose name matches 1-1 with a page field
+    for (var field in batch) {
+        if (nonAutofillFields.indexOf(field) === -1) {
+            $('#meadData_' + field).html(batch[field]);
+        }
+    }
+
+    // populate ingredients
+    batch.ingredients.forEach(function(ingredient) {
+        $('#meadData_ingredients').append('<li>' + ingredient + '</li>');
+    })
+
+    // populate timeline ingredients
+    batch.timeline.forEach(function(entry) {
+        $('#meadData_timeline').append('<li><strong>' + entry[0] + '</strong>: ' + entry[1] + '</li>');
+    })
+
+    // calculate ABV and populate SG
+    var originalGravity = batch.gravReadings[0][1];
+    batch.gravReadings.forEach(function(gravReading) {
+        let abv = Math.round((originalGravity - gravReading[1]) * 13125) / 100;
+        $('#meadData_gravity').append('<li><strong>' + gravReading[0] + '</strong>: ' + gravReading[1] + ' (' + abv + '% ABV); ' + gravReading[2] + '</li>');
+    })
+
+    // dynamically add images to page
+    if(batch.images.length > 0){
+        batch.images.forEach(function(image) {
+            $('#meadData_images').append('<li><img src="img/' + image + '"></li>');
+        });
+
+        $('.slider').slider();
+    }
+
+}
+
+// check if valid batch; return index if so and -1 if not
+function checkValidBatch(batchID) {
+    var batchIDs = meadData.map(function(batch) {
+        return batch.batchID;
+    });
+
+    return batchIDs.indexOf(batchID);
+}
+
+// check if valid batch; return index if so and -1 if not
+function checkValidBottle(bottleID) {
+    for (var i = 0; i < meadData.length; i++) {
+        if (bottleID >= meadData[i].bottleRangeMin && bottleID <= meadData[i].bottleRangeMax) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+// validate ID and call draw function
+function lookup() {
+    var rawID = $('#serial').val();
+    var idType = rawID[0].toUpperCase();
+    var idNumber = Number(rawID.slice(1));
+
+    var dataID = -1;
+    if (idType === 'B') {
+        dataID = checkValidBottle(idNumber);
+    } else if (idType === 'F') {
+        dataID = checkValidBatch(idNumber);
+    }
+
+    if (dataID === -1) {
+        Materialize.toast('Non-existent or malformed ID number; please try again.', 4000);
+        return;
+    }
+
+    drawBatch(meadData[dataID]);
+    $('#dataModal').modal('open');
+}
+
+
+$(document).ready(function() {
+    // initialize modal handling and focus the input field
+    $('.modal').modal();
+    $('input:text:visible:first').focus();
+});
+
+// handle enter keypress in input box
+$("#serial").on('keyup', function(e) {
+    if (e.keyCode == 13) {
+        lookup();
+    }
+});
---
+++ meads.js
@@ -0,0 +1,61 @@
+const meadData = [{
+        'batchID': 1,
+        'type': 'Sweet Mead',
+        'recipe': 'Jutini\'',
+        'bottleRangeMin': 1,
+        'bottleRangeMax': 5,
+        'size': '1 gal.',
+        'notes': 'This is one of my first two meads! This mead uses a weaker yeast that should result in a sweet mead. This is a small batch and may or may not come out tasting like battery acid -- as always, longer aging makes for a better mead! Making .5 gal of starsan soln (1oz/5gal H20 = .1oz/.5gal). Sharing between two bottles. Honey, then yeast + DAP + hulls, then water and agitate for 3 mins.',
+        'ingredients': [
+            '3.5lbs honey (3lb Crocket Raw Arizona Desert Wildflower honey, .5lb Nature Nate\'s Raw Unfiltered honey)',
+            'Tap water to 1 gal.',
+            '1 tsp LD Carlson nutrient (diammonium phosphate)',
+            '.5 tsp LD Carlson energizer (yeast hulls)',
+            '5 grams Safale US-05 yeast',
+            '.5 Campden tablet (crushed); introduced at rerack'
+        ],
+        'timeline': [
+            // date, notes
+            ['2017-10-09 1830', 'Must prepared.'],
+            ['2017-10-09 1930', 'Carboy airlocked.']
+        ],
+        'gravReadings': [
+            // date, OG, notes
+            ['2017-10-09', 1.12, 'Pitched']
+        ],
+        'images': ['mixedmusts.jpg', 'musts.jpg', 'ingredients.jpg'],
+        'tastingNotes': '',
+        'suggestedOpeningDate': '2018-04-21 at the earliest; let age for a year (2019-04) or longer for optimum taste!',
+        'promisedBottles': 'B1-3 JWK, B4 MMa, B5 MBa'
+    },
+    {
+        'batchID': 2,
+        'type': 'Semi-Dry Mead',
+        'recipe': 'Jutini-α',
+        'bottleRangeMin': 6,
+        'bottleRangeMax': 11,
+        'size': '1 gal.',
+        'notes': 'This is one of my first two meads! This mead uses a stronger yeast that should result in a semi-dry mead. This is a small batch and may or may not come out tasting like battery acid -- as always, longer aging makes for a better mead! Making .5 gal of starsan soln (1oz/5gal H20 = .1oz/.5gal). Sharing between two bottles. Honey, then yeast + DAP + hulls, then water and agitate for 3 mins.',
+        'ingredients': [
+            '3.5lbs honey (3lb Crocket Raw Arizona Desert Wildflower honey, .5lb Nature Nate\'s Raw Unfiltered honey)',
+            'Tap water to 1 gal.',
+            '1 tsp LD Carlson nutrient (diammonium phosphate)',
+            '.5 tsp LD Carlson energizer (yeast hulls)',
+            '5 grams Lavlin 71B-1122 yeast',
+            '.5 Campden tablet (crushed); introduced at rerack'
+        ],
+        'timeline': [
+            // date, notes
+            ['2017-10-09 1830', 'Must prepared.'],
+            ['2017-10-09 1930', 'Carboy airlocked.']
+        ],
+        'gravReadings': [
+            // date, OG, notes
+            ['2017-10-09', 1.11, 'Pitched']
+        ],
+        images: ['mixedmusts.jpg', 'f2must.jpg', 'musts.jpg', 'ingredients.jpg', 'f2yeast.jpg'],
+        'tastingNotes': '',
+        'suggestedOpeningDate': '2018-04-21 at the earliest; let age for a year (2019-04) or longer for optimum taste!',
+        'promisedBottles': 'B1-2 JWK, B3 MWe, B4 JFe'
+    }
+];
