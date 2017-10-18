b3475df30c2826a9ddf29a57e58303ff3ff1be92
jkingsman <jack.kingsman@gmail.com>
2017-10-17 18:13:20 -0700

    Make front-facing link better

--- main.js
+++ main.js
@@ -161,10 +161,6 @@ $(document).ready(function() {
     }
 
     drawRecipes();
-
-    window.onhashchange = function() {
-        window.location.reload();
-    };
 });
 
 // handle enter keypress in input box
